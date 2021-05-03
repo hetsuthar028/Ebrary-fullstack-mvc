28fb05e315e5494fd939a2b5488efd0eee80f233
Het Suthar <hetmewada028@gmail.com>
2021-05-03 13:47:20 +0530

    Author Routes/View Added

--- server.js
+++ server.js
@@ -6,13 +6,17 @@ const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
 const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
+const bodyParser = require('body-parser')
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + "/views")
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false}))
 app.use('/', indexRouter)
+app.use('/authors', authorRouter)
 
 const mongoose = require('mongoose')
 mongoose.connect(process.env.DATABASE_URL, {
